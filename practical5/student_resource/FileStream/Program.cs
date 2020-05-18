fd4d555f65eef7f83c7cf3f269c4e7b6ea76bfa0
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2020-05-18 12:56:28 +0800

    added practical5

---
+++ practical5/student_resource/FileStream/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+namespace FileStream
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string path = @"MyTest.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello World");
+                }
+            }
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+            if (path = "abc") {
+                Console.WriteLine("This is very hard to detect");
+            }
+        }
+    }
+}
---
+++ practical5/student_resource/NetStream/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.IO;
+namespace NetStream
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uriBuilder = new UriBuilder("https://www.google.com.sg");
+            Uri uri = uriBuilder.Uri;
+            WebRequest request = WebRequest.Create(uri);
+            using (WebResponse response = request.GetResponse())
+            {
+                var reader = new StreamReader(response.GetResponseStream());
+                string s = reader.ReadToEnd();
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
